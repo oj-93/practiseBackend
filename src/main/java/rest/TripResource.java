b4fd6ec99ecd00ca1ec879d362b1362cab67b0bb
Oliver Jørgensen <cph-oj93@cphbusiness.dk>
2022-01-13 19:54:19 +0100

    backend US6 pending

--- src/main/java/rest/TripResource.java
+++ src/main/java/rest/TripResource.java
@@ -25,7 +25,7 @@ public class TripResource {
 
     @Path("all_trips")
     @GET
-    @RolesAllowed({"user","admin"})
+    @RolesAllowed("user")
     @Produces({MediaType.APPLICATION_JSON})
     public String getAllTrips() {
         List<TripDTO> list = FACADE.getAllTrips();
